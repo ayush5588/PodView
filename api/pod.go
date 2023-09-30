b8af6bb154a110a9ec85a94649254e7f6e4847ef
ayush5588 <ayushsinha126@gmail.com>
2023-09-30 13:22:05 +0530

    Add deployment validation

---
+++ api/pod.go
@@ -0,0 +1,12 @@
+package api
+
+// PodList ...
+type PodList struct {
+	Pods []Pod `json:"pods"`
+}
+
+// Pod ...
+type Pod struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+}
---
+++ pkg/podview/podview.go
@@ -0,0 +1,94 @@
+package podview
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+
+	"github.com/ayush5588/PodView/api"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	v1 "k8s.io/client-go/applyconfigurations/apps/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	// ErrDeploymentNotFound ...
+	ErrDeploymentNotFound = errors.New("No deployment found with the given name")
+)
+
+type podViewClient struct {
+	client.Client
+	deploymentName      string
+	deploymentNamespace string
+}
+
+func NewPodViewClient(c client.Client, deploymentName, deploymentNamespace string) podViewClient {
+	return podViewClient{
+		Client:              c,
+		deploymentName:      deploymentName,
+		deploymentNamespace: deploymentNamespace,
+	}
+}
+
+func (p podViewClient) ValidateDeployment() (v1.Deployment, error) {
+	depName := p.deploymentName
+
+	parsedFieldSelector := fields.ParseSelector(fmt.Sprintf("metadata.name: %s", depName))
+
+	listOptions := &client.ListOptions{
+		FieldSelctor: parsedFieldSelector,
+	}
+
+	if p.deploymentNamespace != "" {
+		listOptions.Namespace = p.deploymentNamespace
+	}
+
+	depList := &v1.DeploymentList{}
+	err := p.Client.List(context.Background(), depList, listOptions)
+	if err != nil {
+		return v1.Deployment{}, err
+	}
+
+	deps := depList.Items
+
+	var wg sync.WaitGroup
+	wg.Add(len(deps))
+
+	var depExist bool
+	var dep v1.Deployment
+
+	for _, d := range deps {
+		go func(d v1.Deployment) {
+			defer wg.Done()
+			// If deployment found -> return
+			if d.Name == depName {
+				dep = d
+				depExist = true
+				return
+			}
+		}(d)
+	}
+
+	wg.Wait()
+
+	if !depExist {
+		return v1.Deployment{}, nil
+	}
+
+	return dep, nil
+
+}
+
+func (p podViewClient) GetPods() (api.PodList, error) {
+	dep, err := p.ValidateDeployment()
+	if err != nil {
+		return api.PodList{}, err
+	}
+
+	if reflect.DeepEqual(dep, v1.Deployment{}) {
+		return api.PodList{}, ErrDeploymentNotFound
+	}
+}
